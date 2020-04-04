20b17d16cc16c5bf8d39ee88b16c8a9d91743469
riskymind <oparaiskelechi@gmail.com>
2020-04-04 14:21:23 +0100

    set up the nav controllers and destinations

--- app/src/main/java/com/thisiskelechi/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/thisiskelechi/navigationcomponent/MainActivity.kt
@@ -2,11 +2,38 @@ package com.thisiskelechi.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupBottomNavMenu(navController)
+        setupSideNavigationMenu(navController)
+        setupActionBar(navController)
+
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        bottom_nav?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setupSideNavigationMenu(navController: NavController) {
+        nav_view?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setupActionBar(navController: NavController) {
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
     }
 }
