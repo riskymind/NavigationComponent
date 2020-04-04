5bab03d5e941d0127c23cf9e1ffda98ba5ff312b
riskymind <oparaiskelechi@gmail.com>
2020-04-04 14:33:40 +0100

    set up the navigation back buttons and menu options

--- app/src/main/java/com/thisiskelechi/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/thisiskelechi/navigationcomponent/MainActivity.kt
@@ -2,19 +2,21 @@ package com.thisiskelechi.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
         setupBottomNavMenu(navController)
         setupSideNavigationMenu(navController)
         setupActionBar(navController)
@@ -35,5 +37,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupActionBar(navController: NavController) {
         NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navigated = NavigationUI.onNavDestinationSelected(item!!, navController)
+        return navigated || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawer_layout)
     }
 }
